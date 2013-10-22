0a2a75925714d25b8a03be99fd286eb446e87ac0
lisa ballard <lballard.cat@gmail.com>
2013-10-22 15:25:43 -0700

    fixes broken writing to file twilio app

--- remote_control.py
+++ remote_control.py
@@ -5,35 +5,36 @@ import twilio.twiml
 
 app = Flask(__name__)
 
+f = open('status.txt')
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     """Respond to incoming calls with a simple text message."""
 
-    resp = twilio.twiml.Response()
-
     text_command = request.values.get('Body', None)
-    text_command = text_command.strip().upper()
+
+    resp = twilio.twiml.Response()
 
     if text_command and text_command not in ['ON','OFF','CAL','CALIBRATE']:
         resp.message("invalid command: " + text_command)
 
     elif text_command:
         # write text command to file
-        print(text_command, file='status.txt')
+        print(text_command, file=f)
         resp.message("Cat Detector status changed to: " + text_command)
 
     else:
         # no text_command, just try to get current status
         try:
             status = open('status.txt').readlines()[0].strip()
+            resp.message(status)
         except IOError:
             # default status is 'ON'
             status = 'ON'
-            print(status, file='status.txt')
+            print(status, file=f)
             resp.message(status)
 
     return str(resp)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
