1eeacfdb53eb7c3be38d61807b05c3b7f4e10a7a
lisa ballard <lballard.cat@gmail.com>
2013-10-31 12:38:02 -0700

    adds a template for the secrets.py file

---
+++ secrets_template.py
@@ -0,0 +1,8 @@
+# secrets.py
+secrets = {
+    'gmail_addy': '',  # used for sending the text to sms_recipients
+    'sms_recipients': [''],  # phone number
+    'aws_access_key_id': '',
+    'aws_bucket_name': '',
+    'aws_secret_access_key': ''
+}
