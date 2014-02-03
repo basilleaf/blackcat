35b39f416280a1bae87f2cba8a80fa25ca289e53
lisa ballard <lballard.cat@gmail.com>
2014-02-02 19:07:17 -0800

    refactors calibrate.py a little for easier reading/using

--- calibrate.py
+++ calibrate.py
@@ -3,6 +3,27 @@ from __future__ import print_function
 from datetime import datetime
 from time import strftime, mktime, sleep
 
+def get_trigger_limit(base):
+    # ambient lighting effects rate of false alarms
+    # tweek this method according to observation
+    # right now we have false alarm around noon on sunny days without any cat
+    # zero false positives woot!
+
+    # the sensor deamon runs this:
+    # if int(reading) < (base-trigger_limit):
+    #   trigger alarm
+    #
+    # (note that 'trigger_limit' may still be referred to as 'variance' elsewhere)
+    if base > 350:
+       trigger_limit = int(base/4)  # based on observation, something about noontime sun
+    if base > 125:
+        trigger_limit = int(base/2)
+    else:
+        trigger_limit = int(base/4)
+
+    return trigger_limit
+
+
 def calibrate(ser, f):
 
     print("recalibrating..")
@@ -14,23 +35,18 @@ def calibrate(ser, f):
         latest_readings.append(int(reading))
         sleep(.3)
 
-    # and recalculate base and variance
+    # and recalculate base and trigger_limit/trigger_limit
+
     base = int(sum(latest_readings)/len(latest_readings))
 
-    # the sensor becomes more sensitive in brighter ambient lighting
-    if base > 350:
-	   variance = int(base/4)  # based on observations
-    if base > 125:
-        variance = int(base/2)
-    else:
-        variance = int(base/4)
+    trigger_limit = get_trigger_limit(base)
 
     time_last_calib = mktime(datetime.now().timetuple())
 
     print("recalibrated " + datetime.fromtimestamp(mktime(datetime.now().timetuple())).strftime('%Y-%m-%d %H:%M:%S').strip())
-    print('base: ' + str(base) + ' ' + 'variance: ' + str(variance))
+    print('base: ' + str(base) + ' ' + 'trigger_limit: ' + str(trigger_limit))
 
     # debug
-    # print('%s %s %s' % (str(base), str(variance), str(time_last_calib)))
+    # print('%s %s %s' % (str(base), str(trigger_limit), str(time_last_calib)))
 
-    return (base, variance, time_last_calib)
+    return (base, trigger_limit, time_last_calib)
