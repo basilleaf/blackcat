47072479762c8fe1a3e085b31218e65eb6d03e50
lisa ballard <lballard.cat@gmail.com>
2014-02-09 23:11:17 -0800

    fixed bug when serial line does not agree to be cast as int and some formatting changes

--- calibrate.py
+++ calibrate.py
@@ -1,27 +1,9 @@
 # calibrate
 from __future__ import print_function
+import sys
+
 from datetime import datetime
 from time import strftime, mktime, sleep
-
-def get_trigger_limit(base):
-    # ambient lighting effects rate of false alarms
-    # tweek this method according to observation
-    # right now we have false alarm around noon on sunny days without any cat
-    # zero false positives woot!
-
-    # the sensor deamon runs this:
-    # if int(reading) < (base-trigger_limit):
-    #   trigger alarm
-    #
-    # (note that 'trigger_limit' may still be referred to as 'variance' elsewhere)
-    if base > 350:
-       trigger_limit = int(base/4)  # based on observation, something about noontime sun
-    if base > 125:
-        trigger_limit = int(base/2)
-    else:
-        trigger_limit = int(base/4)
-
-    return trigger_limit
 
 
 def calibrate(ser, f):
@@ -30,22 +12,33 @@ def calibrate(ser, f):
 
     # take 10 readings
     latest_readings = []
+    error_count = 0
     for c in range(0,5):
         reading = ser.readline()
-        latest_readings.append(int(reading))
+        try:
+            latest_readings.append(int(reading))
+        except ValueError:
+            # how many time will we let this trip?
+            error_count += 1
+            if error_count > 5:
+                print("could not calibrate, errors in reading serial line as int")
+                sys.exit()
 
-    # and recalculate base and trigger_limit/trigger_limit
-
+    # and recalculate base and variance
     base = int(sum(latest_readings)/len(latest_readings))
 
-    trigger_limit = get_trigger_limit(base)
+    # the sensor becomes less sensitive in brighter ambient lighting
+    if base > 100:
+    	variance = base/2
+    else:
+	   variance  = base/4
 
     time_last_calib = mktime(datetime.now().timetuple())
 
     print("recalibrated " + datetime.fromtimestamp(mktime(datetime.now().timetuple())).strftime('%Y-%m-%d %H:%M:%S').strip())
-    print('base: ' + str(base) + ' ' + 'trigger_limit: ' + str(trigger_limit))
+    print('base: ' + str(base) + ' ' + 'variance: ' + str(variance))
 
     # debug
-    # print('%s %s %s' % (str(base), str(trigger_limit), str(time_last_calib)))
+    # print('%s %s %s' % (str(base), str(variance), str(time_last_calib)))
 
-    return (base, trigger_limit, time_last_calib)
+    return (base, variance, time_last_calib)
