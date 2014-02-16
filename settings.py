f2cb7e00605fa97fb248a813068dfe0709e14954
lisa ballard <lballard.cat@gmail.com>
2014-02-16 00:45:34 -0800

    changes serial port setting since pi gave it new port randomly

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 # serial port setting depends on platform
 try:
     import  RPi.GPIO  # try to import the raspberry pi GPIO lib
-    serial_port = '/dev/ttyACM0' # this is a raspberry pi!
+    serial_port = '/dev/ttyACM1' # this is a raspberry pi!
 except ImportError:
     # this must be an aging macbook air
     serial_port = '/dev/tty.usbmodemfd121'
